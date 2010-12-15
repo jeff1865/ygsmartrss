511cc526e52066208eab89d6fcc97ff15e2a355b
gonni21c <gonni21c@gmail.com>
2010-12-15 09:30:02 +0000

    Daily Update (Web API part)

---  --username gonni21c/RssGenerator/src/com/ygsoft/rss/ui/web/GdpMain.java
+++  --username gonni21c/RssGenerator/src/com/ygsoft/rss/ui/web/GdpMain.java
@@ -1,14 +1,38 @@
 package com.ygsoft.rss.ui.web;
 
+import java.io.IOException;
+
+import org.ygsoft.webserver.ProtocolHelper;
 import org.ygsoft.webserver.XRequest;
 import org.ygsoft.webserver.XResponse;
 import org.ygsoft.webserver.service.Gdplet;
 
-public class GdpMain extends Gdplet {
+import com.ygsoft.rss.SmartRssService;
 
+public class GdpMain extends Gdplet {
+	
+	private SmartRssService smSvc = null;
+	
+	public GdpMain (SmartRssService smartRssService){
+		this.smSvc = smartRssService;
+	}
+	
 	@Override
 	protected void service(XRequest req, XResponse res) {
-		// TODO Auto-generated method stub
+		try {
+			res.setAttribute(ProtocolHelper.ContentType, ProtocolHelper.ValueContentTypeText);
+			res.writeStringToHeader();
+			
+			ViewTemplate vt = new ViewTemplate();
+			vt.setTitle("Smart RSS :: Main");
+			String bodyText = "This is test Message";
+			vt.setMainSource(bodyText);
+			
+			res.writeStringToBody(vt.getSourceText());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
---  --username gonni21c/RssGenerator/src/com/ygsoft/rss/ui/web/WebMain.java
+++  --username gonni21c/RssGenerator/src/com/ygsoft/rss/ui/web/WebMain.java
@@ -1,15 +1,40 @@
 package com.ygsoft.rss.ui.web;
 
-import org.ygsoft.webserver.XRequest;
-import org.ygsoft.webserver.XResponse;
+import org.ygsoft.webserver.MicroWebServer;
+import org.ygsoft.webserver.PLogging;
+import org.ygsoft.webserver.ServiceManager;
+import org.ygsoft.webserver.ServiceMapper;
+import org.ygsoft.webserver.ServiceScheduler;
+import org.ygsoft.webserver.service.GdpService;
 import org.ygsoft.webserver.service.Gdplet;
+import org.ygsoft.webserver.service.StaticResourceService;
 
-public class WebMain extends Gdplet {
-
-	@Override
-	protected void service(XRequest req, XResponse res) {
-		// TODO Auto-generated method stub
+public class WebMain {
+	
+	private static int serverPort = 2012;
+	
+	public static void main(String ... v){
+		System.out.println("Start RssGen WebMain..");
+		// V2
+		ServiceMapper fCont = new ServiceMapper();
 		
+		ServiceManager<Gdplet> sMng = new ServiceManager<Gdplet>();
+		sMng.addService(new GdpMain(null));
+//		sMng.addService(new Hello());
+//		sMng.addService(new Exam());
+//		sMng.addService(new FileTransExam());
+		
+		// register & active GDP service
+		fCont.addContainer(new GdpService(sMng));
+		
+		// register & active static resource service
+		//fCont.addContainer(new StaticResourceService("HttpRoot/"));
+		PLogging.printv(PLogging.DEBUG, fCont.getContainers().size() + " containers registered ..");
+		
+		MicroWebServer server = new MicroWebServer(serverPort, fCont, ServiceScheduler.getDefaultScheduler());
+		server.startServer();
+		
+		PLogging.printv(PLogging.DEBUG, "YG XServer stopped..");
 	}
 
 }
